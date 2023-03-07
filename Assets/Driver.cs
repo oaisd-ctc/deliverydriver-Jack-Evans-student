b94c392b5f87528de2f4e41f61009ec13afac801
Jack-Evans-student <112406787+Jack-Evans-student@users.noreply.github.com>
2023-03-07 08:51:23 -0500

    cloned

---
+++ Assets/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+   
+    void Start()
+    {
+        transform.Rotate(0, 0, 45);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
